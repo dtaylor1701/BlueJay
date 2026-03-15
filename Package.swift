024d365c4f6bd8cf91a86a8e0ceeb5c2a1d2e77e
David Taylor <dktaylor1701@gmail.com>
2026-03-15 15:09:22 -0400

    Updated packages

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
       targets: ["BlueJay"])
   ],
   dependencies: [
-    .package(url: "https://github.com/dtaylor1701/Goose.git", .upToNextMajor(from: "1.0.0"))
+    .package(path: "../Goose")
   ],
   targets: [
     // Targets are the basic building blocks of a package, defining a module or a test suite.
